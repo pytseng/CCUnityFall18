f5d13f5cac7e9ec962ba7688727d8f4bef1a66a0
g-bunny <miri@newschool.edu>
2018-09-14 10:43:17 -0400

    Week3: Vector math, drawing vectors, vector movement, getAxis, getMouseButton

---
+++ Assets/Scripts/VectorMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorMovement : MonoBehaviour
+{
+    Vector3 pos;
+    Vector3 vel;
+    Vector3 clickPos;
+    Vector3 translatedClick;
+
+    public Camera cam;
+
+    float speedMult = 3f;
+    void Start()
+    {
+        pos = this.transform.position;
+        //vel = new Vector3(-0.1f, 0, 0);
+    }
+
+    void FixedUpdate()
+    {
+        MouseMove();
+    }
+
+    void KeyMove(){
+        //if(Input.GetKey(KeyCode.LeftArrow)){
+        //    pos += vel;
+        //}
+        vel = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0); //* speedMult;
+        this.GetComponent<Rigidbody>().velocity = vel;
+        //pos = pos + vel;
+        //this.transform.position = pos;
+    }
+    void MouseMove(){
+        if(Input.GetMouseButton(0)){
+            //Debug.Log("Click");
+            Debug.Log("clickPos: " + clickPos);
+            clickPos = Input.mousePosition;
+            translatedClick = cam.ScreenToWorldPoint(new Vector3(clickPos.x, clickPos.y, cam.nearClipPlane));
+            Debug.Log("translatedClick: " + translatedClick);
+            Debug.DrawRay(Vector3.zero, translatedClick, Color.cyan);
+
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(translatedClick.x, translatedClick.y * speedMult, 0f));
+        }
+    }
+}
---
+++ Assets/Scripts/VectorVisualizer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorVisualizer : MonoBehaviour {
+    Vector3 vecA;
+    Vector3 vecB;
+    float mag;
+    Vector3 normalA;
+
+	void Start () {
+        vecA = new Vector3(3, 6, 9);
+        vecB = new Vector3(-2, 2, 8);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //DrawVectorMath();
+        DrawTransformVectors();
+    }
+
+    void DrawVectorMath(){
+        Debug.DrawRay(Vector3.zero, vecA, Color.blue);
+        Debug.DrawRay(Vector3.zero, vecB, Color.red);
+        //magnitude = sqrt(x^2 + y^2 + z^2)
+        mag = Mathf.Sqrt(vecA.x * vecA.x + vecA.y * vecA.y + vecA.z * vecA.z);
+        //Mathf.Pow(vecA.x, 2f)
+
+        Debug.Log("mag: " + mag); //;print();
+        Debug.Log("this is Unity mag: " + vecA.magnitude);
+        //print("mag" + mag);
+
+        //normal vector = yourVector / magnitude
+        normalA = vecA / mag;
+        Debug.Log("normal: " + normalA);
+        Debug.Log("Unity built in normal: " + vecA.normalized);
+        //Debug.DrawRay(Vector3.zero, normalA * 20f);
+        Debug.DrawRay(Vector3.zero, normalA, Color.green);
+
+        //Addition
+        Debug.DrawRay(Vector3.zero, vecA + vecB, Color.gray);
+
+        //Subtraction
+        Vector3 AMinusB = vecA - vecB;
+        Vector3 BMinusA = vecB - vecA;
+
+        Debug.DrawRay(vecA, BMinusA, Color.yellow);
+        Debug.DrawRay(vecB, AMinusB, Color.magenta);
+    }
+
+    void DrawTransformVectors(){
+        Debug.DrawRay(transform.position, transform.right, Color.green);
+        Debug.DrawRay(transform.position, transform.up, Color.red);
+        Debug.DrawRay(transform.position, transform.forward, Color.blue);
+    }
+}
