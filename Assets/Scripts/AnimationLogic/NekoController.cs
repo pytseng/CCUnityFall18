d786ed3af83c4f7514756e6db83b6d7d80980c27
g-bunny <miri@newschool.edu>
2018-10-11 21:56:02 -0400

    Week7: setting up 2D and 3D animations, adding logic for walk & jump

---
+++ Assets/Scripts/AnimationLogic/NekoController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NekoController : MonoBehaviour {
+    float velX;
+    float velY;
+    Animator myAnim;
+    Rigidbody rb;
+
+	void Start () {
+        myAnim = this.GetComponent<Animator>();
+        rb = this.GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        if(Time.time >= 3f && Time.time <= 3.1f){
+            myAnim.Play("WhiteCat");
+        }
+
+        velX = Input.GetAxis("Horizontal");
+        velY = Input.GetAxis("Vertical");
+
+        myAnim.SetFloat("VelX", velX);
+        myAnim.SetFloat("VelY", velY);
+	}
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(velX, velY, 0f);
+    }
+}
