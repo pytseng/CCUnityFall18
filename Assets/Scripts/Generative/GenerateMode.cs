cd889b4212d6647cda9f3bcc13511cc72dbf9f51
g-bunny <miri@newschool.edu>
2018-11-01 21:19:47 -0400

    Week 10: Generative, prefabs, modulo, enums

---
+++ Assets/Scripts/Generative/GenerateMode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMode : MonoBehaviour {
+    public enum Mode
+    {
+        rain = 0,
+        grid
+    };
+
+   public Mode myMode;
+
+    int mode = 0; // if mode = 0, generate rain... if mode = 1 generate grid... if mode = 2 generate cats;
+
+	void Update () {
+        if(myMode == Mode.rain){
+            GetComponent<GenerateRain>().enabled = true;
+            //GetComponentInChildren<Falling>().RunFall = true;
+            GetComponent<GenerateGrid>().enabled = false;
+        }
+        else if(myMode == Mode.grid){
+            GetComponent<GenerateGrid>().enabled = true;
+            GetComponent<GenerateRain>().enabled = false;
+            //GetComponentInChildren<Falling>().RunFall = false;
+        }
+	}
+}
---
+++ Assets/Scripts/Generative/GenerateRain.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateRain : MonoBehaviour {
+    [SerializeField] GameObject rain;
+    GameObject[] rainObjectSet = new GameObject[50];
+    int numRainDrops = 50;
+    Vector3 startingPos;
+    public bool RunRain;
+
+    private void Awake()
+    {
+        this.enabled = false;
+    }
+    void OnEnable()
+    {
+        for (int i = 0; i < numRainDrops; i++)
+        {
+            startingPos = new Vector3(Random.Range(-30f, 30f), 10, -10);
+            rainObjectSet[i] = Instantiate(rain, this.transform) as GameObject;
+        }
+    }
+}
